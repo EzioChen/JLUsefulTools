66e599b43ac9c24b6c46595d88e9e9fb3735902f
eziochan <jackenwind@163.com>
2024-03-21 14:26:27 +0800

    remove uikit

---
+++ Example/JLUsefulTools/JLUsefulTools.h
@@ -0,0 +1,19 @@
+//
+//  JLUsefulTools.h
+//  JLUsefulTools
+//
+//  Created by EzioChan on 2024/3/21.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JLUsefulTools.
+FOUNDATION_EXPORT double JLUsefulToolsVersionNumber;
+
+//! Project version string for JLUsefulTools.
+FOUNDATION_EXPORT const unsigned char JLUsefulToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLUsefulTools/PublicHeader.h>
+
+
--- Example/Pods/Target Support Files/Pods-JLUsefulTools_Example/Pods-JLUsefulTools_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-JLUsefulTools_Example/Pods-JLUsefulTools_Example-frameworks.sh
@@ -182,7 +182,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyAttributes/SwiftyAttributes.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
@@ -191,7 +190,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyAttributes/SwiftyAttributes.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
