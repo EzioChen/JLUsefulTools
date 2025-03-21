3eb4ce96993b1614edacea8932e9a57789849142
eziochan <jackenwind@163.com>
2025-03-21 14:55:32 +0800

    1.增加一个任务 Promise

---
+++ JLUsefulTools/Classes/Thread/EcTaskPromise.swift
@@ -0,0 +1,67 @@
+//
+//  EcTaskPromise.swift
+//  JLUsefulTools
+//
+//  Created by EzioChan on 2025/3/21.
+//
+
+
+import UIKit
+
+@objcMembers
+public class EcTaskPromise<T> {
+    public typealias Completion = (T) -> Void
+    private var result: T?
+    private var callbacks: [Completion] = []
+    private var errorHandler: ((Error) -> Void)?
+
+    public init(_ work: (@escaping Completion) -> Void) {
+        work { value in
+            self.result = value
+            DispatchQueue.main.async {
+                self.callbacks.forEach { $0(value) }
+            }
+        }
+    }
+
+    @discardableResult
+    public func then(_ callback: @escaping Completion) -> JLTaskPromise<T> {
+        if let result = result {
+            DispatchQueue.main.async {
+                callback(result)
+            }
+        } else {
+            callbacks.append(callback)
+        }
+        return self
+    }
+
+    @discardableResult
+    public func then<U>(_ transform: @escaping (T) throws -> JLTaskPromise<U>) -> JLTaskPromise<U> {
+        return JLTaskPromise<U> { fulfill in
+            self.then { value in
+                do {
+                    let nextPromise = try transform(value)
+                    nextPromise.then { nextValue in
+                        fulfill(nextValue)
+                    }
+                } catch {
+                    self.reject(error)
+                }
+            }
+        }
+    }
+
+    @discardableResult
+    public func `catch`(_ handler: @escaping (Error) -> Void) -> Self {
+        errorHandler = handler
+        return self
+    }
+
+    private func reject(_ error: Error) {
+        DispatchQueue.main.async {
+            self.errorHandler?(error)
+        }
+    }
+}
+
